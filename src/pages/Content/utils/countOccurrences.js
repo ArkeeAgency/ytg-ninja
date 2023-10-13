5011184600591034f89b8ee7a3b0d47c3c93d2cb
Antoine Kingue <contact@antoinek.fr>
2023-10-13 13:14:49 +0200

    🎨 improve count occurences util regex

--- src/pages/Content/utils/countOccurrences.js
+++ src/pages/Content/utils/countOccurrences.js
@@ -1,5 +1,7 @@
 function countOccurrences(text, key) {
-  const regex = new RegExp(key, "gi");
+  // Escape special characters in the key and use word boundaries with consideration for apostrophes
+  const escapedKey = key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const regex = new RegExp(`(?:\\b|\\s|['])${escapedKey}(?:\\b|\\s|['])`, "gi");
   const matches = text.match(regex);
 
   return matches ? matches.length : 0;
